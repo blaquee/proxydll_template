4870f3725cce9ed8e3d77f9785baac34e80fdc5f
zeffy <zeffy@users.noreply.github.com>
2017-07-25 15:09:54 -0700

    remove braces

--- proxydll_template/dllmain.c
+++ proxydll_template/dllmain.c
@@ -5,10 +5,8 @@
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
-    {
         DisableThreadLibraryCalls(hModule);
         break;
-    }
     case DLL_PROCESS_DETACH:
         if (lpReserved == NULL) {
             proxy_dll_free();
