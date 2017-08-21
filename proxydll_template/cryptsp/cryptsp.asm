5c985e67c658806f29db0433615ba4bf32235daf
zeffy <zeffy@users.noreply.github.com>
2017-08-21 11:46:37 -0700

    combine x86 and x64 asm using macros

---
+++ proxydll_template/cryptsp/cryptsp.asm
@@ -0,0 +1,114 @@
+ifndef x64
+    .model flat
+endif
+
+.code
+
+ifndef x64
+    resolve_export_proc proto C arg1:dword
+else
+    extern resolve_export_proc : proc
+endif
+
+M_EXPORT_PROC macro export, index
+export proc
+ifndef x64
+    invoke resolve_export_proc, index
+    jmp proc ptr eax
+else
+    push rcx
+if index eq 0
+    xor rcx, rcx
+else
+    mov rcx, index
+endif
+    call resolve_export_proc
+    pop rcx
+    jmp proc ptr rax
+endif
+export endp
+endm
+
+M_EXPORT_PROC CheckSignatureInFile, 0
+
+M_EXPORT_PROC CryptAcquireContextA, 1
+
+M_EXPORT_PROC CryptAcquireContextW, 2
+
+M_EXPORT_PROC CryptContextAddRef, 3
+
+M_EXPORT_PROC CryptCreateHash, 4
+
+M_EXPORT_PROC CryptDecrypt, 5
+
+M_EXPORT_PROC CryptDeriveKey, 6
+
+M_EXPORT_PROC CryptDestroyHash, 7
+
+M_EXPORT_PROC CryptDestroyKey, 8
+
+M_EXPORT_PROC CryptDuplicateHash, 9
+
+M_EXPORT_PROC CryptDuplicateKey, 10
+
+M_EXPORT_PROC CryptEncrypt, 11
+
+M_EXPORT_PROC CryptEnumProviderTypesA, 12
+
+M_EXPORT_PROC CryptEnumProviderTypesW, 13
+
+M_EXPORT_PROC CryptEnumProvidersA, 14
+
+M_EXPORT_PROC CryptEnumProvidersW, 15
+
+M_EXPORT_PROC CryptExportKey, 16
+
+M_EXPORT_PROC CryptGenKey, 17
+
+M_EXPORT_PROC CryptGenRandom, 18
+
+M_EXPORT_PROC CryptGetDefaultProviderA, 19
+
+M_EXPORT_PROC CryptGetDefaultProviderW, 20
+
+M_EXPORT_PROC CryptGetHashParam, 21
+
+M_EXPORT_PROC CryptGetKeyParam, 22
+
+M_EXPORT_PROC CryptGetProvParam, 23
+
+M_EXPORT_PROC CryptGetUserKey, 24
+
+M_EXPORT_PROC CryptHashData, 25
+
+M_EXPORT_PROC CryptHashSessionKey, 26
+
+M_EXPORT_PROC CryptImportKey, 27
+
+M_EXPORT_PROC CryptReleaseContext, 28
+
+M_EXPORT_PROC CryptSetHashParam, 29
+
+M_EXPORT_PROC CryptSetKeyParam, 30
+
+M_EXPORT_PROC CryptSetProvParam, 31
+
+M_EXPORT_PROC CryptSetProviderA, 32
+
+M_EXPORT_PROC CryptSetProviderExA, 33
+
+M_EXPORT_PROC CryptSetProviderExW, 34
+
+M_EXPORT_PROC CryptSetProviderW, 35
+
+M_EXPORT_PROC CryptSignHashA, 36
+
+M_EXPORT_PROC CryptSignHashW, 37
+
+M_EXPORT_PROC CryptVerifySignatureA, 38
+
+M_EXPORT_PROC CryptVerifySignatureW, 39
+
+M_EXPORT_PROC SystemFunction035, 40
+
+end
---
+++ proxydll_template/version/version.asm
@@ -0,0 +1,62 @@
+ifndef x64
+    .model flat
+endif
+
+.code
+
+ifndef x64
+    resolve_export_proc proto C arg1:dword
+else
+    extern resolve_export_proc : proc
+endif
+
+M_EXPORT_PROC macro export, index
+export proc
+ifndef x64
+    invoke resolve_export_proc, index
+    jmp proc ptr eax
+else
+    push rcx
+if index eq 0
+    xor rcx, rcx
+else
+    mov rcx, index
+endif
+    call resolve_export_proc
+    pop rcx
+    jmp proc ptr rax
+endif
+export endp
+endm
+
+M_EXPORT_PROC GetFileVersionInfoA, 0
+
+M_EXPORT_PROC GetFileVersionInfoByHandle, 1
+
+M_EXPORT_PROC GetFileVersionInfoExW, 2
+
+M_EXPORT_PROC GetFileVersionInfoSizeA, 3
+
+M_EXPORT_PROC GetFileVersionInfoSizeExW, 4
+
+M_EXPORT_PROC GetFileVersionInfoSizeW, 5
+
+M_EXPORT_PROC GetFileVersionInfoW, 6
+
+M_EXPORT_PROC VerFindFileA, 7
+
+M_EXPORT_PROC VerFindFileW, 8
+
+M_EXPORT_PROC VerInstallFileA, 9
+
+M_EXPORT_PROC VerInstallFileW, 10
+
+M_EXPORT_PROC VerLanguageNameA, 11
+
+M_EXPORT_PROC VerLanguageNameW, 12
+
+M_EXPORT_PROC VerQueryValueA, 13
+
+M_EXPORT_PROC VerQueryValueW, 14
+
+end
